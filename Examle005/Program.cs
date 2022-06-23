faec59fe2a20c6f78ae3c8093edc43c53cf22e16
«Olga» <elfica@yandex.ru>
2022-06-23 19:51:13 +0800

    Условие if else

---
+++ Examle005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if  (username.ToLower() == "ася")
+{
+Console.WriteLine("Привет, Солнце");
+
+}
+else
+{
+   Console.WriteLine ("Привет, "); 
+   Console.WriteLine (username); 
+}
