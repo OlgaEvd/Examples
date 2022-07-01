5202456639ab28a964dd71a27bdf8bf44cb4576e
«Olga» <elfica@yandex.ru>
2022-07-01 19:17:22 +0800

    массив сортировка

---
+++ arr/Program.cs
@@ -0,0 +1,41 @@
+﻿
+int[] arr = {5,8,9,45,78,2,7,9};
+
+void PrintArr(int[] array)
+{
+ int count  = array.Length;
+
+ for(int i = 0; i < count; i++)
+ {
+   Console.Write($"{array[i]} ");
+
+ }
+ Console.WriteLine();
+}
+
+PrintArr(arr);
+
+void SelectionSort(int[] array)
+{
+for(int i = 0; i < array.Length - 1; i++)
+{
+int minPosition = i;
+for(int j = i + 1; j < array.Length; j++)
+{
+if(array[j] < array[minPosition])
+{
+    minPosition = j;
+}
+}
+ int temp = array[i];
+ array[i] = array[minPosition];
+ array[minPosition] = temp;
+
+}
+
+}
+
+
+SelectionSort(arr);
+
+PrintArr(arr);
